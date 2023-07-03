80ec027743b0509f1a9e0c62fce0f495fa8de3c7
MartTH <118419696+MartTH@users.noreply.github.com>
2023-07-04 01:11:27 +0700

    Week02

---
+++ resources/views/test/bird.blade.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        <h1>Callery of Therapong 64122420017</h1>
+        bird<img src="https://images.indianexpress.com/2021/03/falcon-anthony-mackie-1200.jpg" alt="bird" width="300" height="300">
+    </body>
+</html>
---
+++ resources/views/test/cat.blade.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        <h1>Callery of Therapong 64122420017</h1>
+        cat<img src="http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg" alt="cat" width="300" height="300">
+    </body>
+</html>
