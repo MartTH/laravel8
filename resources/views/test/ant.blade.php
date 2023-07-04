7c26a3da6a4b164d55f093ca5c83b3cb1086e3ac
MartTH <118419696+MartTH@users.noreply.github.com>
2023-07-04 17:20:58 +0700

    Week03

--- resources/views/test/ant.blade.php
+++ resources/views/test/ant.blade.php
@@ -1,6 +1,6 @@
 <html>
     <body>
         <h1>Callery of Therapong 64122420017</h1>
-        ant<img src="https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg" alt="ant" width="300" height="300">
+        ant<img src="{{ $ant }}" width="300" height="300">
     </body>
 </html>
