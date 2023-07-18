2a17e9da55f4e8f5eab2adf121411f270f7a19b0
MartTH <118419696+MartTH@users.noreply.github.com>
2023-07-18 16:40:05 +0700

    Week05

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Covid19Controller;
 use App\Http\Controllers\MyProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -42,7 +43,7 @@ Route::get("/blog/{id}/edit", function ($id) {
     return "<h1>This is blog page : {$id} for edit</h1>";
 });
 
-Route::get("/product/{a}/{b}/{c}", function ($a,$b,$c) {
+Route::get("/product/{a}/{b}/{c}", function ($a, $b, $c) {
     return "<h1>This is product page </h1><div> {$a}, {$b}, {$c}</div>";
 });
 
@@ -51,10 +52,10 @@ Route::get("/category/{a?}", function ($a = "mobile") {
 });
 
 Route::get("/myorder/create", function () {
-    return "<h1>This is order form ". request("id")." page : ". request("username") ."</h1>";
+    return "<h1>This is order form " . request("id") . " page : " . request("username") . "</h1>";
 });
 
-Route::get("/hello", function () {	
+Route::get("/hello", function () {
     return view("hello");
 });
 
@@ -63,21 +64,21 @@ Route::get("/good_route", function () {
 });
 
 Route::get("/greeting", function () {
-    
+
     $name = 'James';
     $last_name = 'Mars';
 
-    return view('greeting', compact('name','last_name') );
+    return view('greeting', compact('name', 'last_name'));
 });
 
-Route::get( "/gallery" , function(){
-	$ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
-    $bird = "https://images.indianexpress.com/2021/03/falcon-anthony-mackie-1200.jpg"; 
+Route::get("/gallery", function () {
+    $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
+    $bird = "https://images.indianexpress.com/2021/03/falcon-anthony-mackie-1200.jpg";
     $cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
-    $god = "https://www.blackoutx.com/wp-content/uploads/2021/04/Thor.jpg"; 
-    $spider = "https://icdn5.digitaltrends.com/image/spiderman-far-from-home-poster-2-720x720.jpg"; 
+    $god = "https://www.blackoutx.com/wp-content/uploads/2021/04/Thor.jpg";
+    $spider = "https://icdn5.digitaltrends.com/image/spiderman-far-from-home-poster-2-720x720.jpg";
 
-    return view("test/index", compact("ant","bird","cat","god","spider") );
+    return view("test/index", compact("ant", "bird", "cat", "god", "spider"));
 });
 
 Route::get("/gallery/ant", function () {
@@ -95,16 +96,16 @@ Route::get("/gallery/cat", function () {
     return view("test/cat", compact("cat"));
 });
 
-Route::get("/teacher" , function (){
-	return view("teacher");
+Route::get("/teacher", function () {
+    return view("teacher");
 });
 
-Route::get("/student" , function (){
-	return view("student");
+Route::get("/student", function () {
+    return view("student");
 });
 
-Route::get("/theme" , function (){
-	return view("theme");
+Route::get("/theme", function () {
+    return view("theme");
 });
 
 // Route Template Inheritance
@@ -127,11 +128,13 @@ Route::get('/tables', function () {
     return view('tables');
 });
 
-Route::get("/myprofile/create",[ MyProfileController::class , "create" ]);
-Route::get("/myprofile/{id}/edit", [ MyProfileController::class , "edit" ] );
-Route::get("/myprofile/{id}", [ MyProfileController::class , "show" ]);
-Route::get( "/coronavirus" ,[ MyProfileController::class , "coronavirus" ] );
+Route::get("/myprofile/create", [MyProfileController::class, "create"]);
+Route::get("/myprofile/{id}/edit", [MyProfileController::class, "edit"]);
+Route::get("/myprofile/{id}", [MyProfileController::class, "show"]);
+Route::get("/coronavirus", [MyProfileController::class, "coronavirus"]);
 
-Route::get( "/newgallery" , [ MyProfileController::class , "gallery" ] );
-Route::get( "/newgallery/ant" , [ MyProfileController::class , "ant" ] );
-Route::get( "/newgallery/bird" , [ MyProfileController::class , "bird" ] );
+Route::get("/newgallery", [MyProfileController::class, "gallery"]);
+Route::get("/newgallery/ant", [MyProfileController::class, "ant"]);
+Route::get("/newgallery/bird", [MyProfileController::class, "bird"]);
+
+Route::get('/covid19', [Covid19Controller::class, "index"]);
